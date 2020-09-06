0b36f547cf916d1cd1391881bb0958817d3a6559
Anatoliy Yakimov <yakimov.tolya@gmail.com>
2020-09-06 15:38:39 +0300

    Init repo

---
+++ src/test/java/ru/yakimov/aviato/AviatoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yakimov.aviato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
